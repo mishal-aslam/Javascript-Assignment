c6ac69f8986c5513292a547622e41f3dde17947a
Mishal Aslam <101046955+mishal-aslam@users.noreply.github.com>
2023-01-11 00:17:56 +0500

    ass

---
+++ ass#1/index.js
@@ -0,0 +1,8 @@
+let myNum = 17;  //method#1
+console.log(myNum);
+
+var myNum1 = 21; //method#2
+console.log(myNum1);
+
+const myNum2 = 34; //method#3
+console.log(myNum2);
---
+++ ass#2/index.js
@@ -0,0 +1,20 @@
+let myNum = 17;  //method#1
+console.log(myNum);
+
+myNum = 19;   // Reassigning the value 
+console.log(myNum);
+
+var myNum1 = 21; //method#2
+console.log(myNum1);
+
+myNum1 = 12;   // Reassigning the value 
+console.log(myNum1);
+
+
+const myNum2 = 34; //method#3
+console.log(myNum2);
+
+myNum2 = 11;   // Reassigning the value 
+console.log(myNum2);  // error
+
+
